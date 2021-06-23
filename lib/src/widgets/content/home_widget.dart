c1d884d3c6920829aa08c27f8b8db7170af82dde
quantum6 <arielgordillolucas@gmail.com>
2021-06-22 22:16:05 -0500

    Presetnacion

--- lib/src/widgets/content/home_widget.dart
+++ lib/src/widgets/content/home_widget.dart
@@ -8,7 +8,7 @@ class HomeWidget extends StatefulWidget {
   @override
   _HomeWidgetState createState() => _HomeWidgetState();
 }
-
+ 
 class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
